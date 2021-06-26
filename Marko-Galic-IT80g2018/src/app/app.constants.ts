effae7d3923a9741773f74542834e60d33d0d861
Marko Galic <56907204+Sharr0@users.noreply.github.com>
2021-06-26 10:48:17 +0200

    Delete Marko-Galic-IT80g2018 directory

--- Marko-Galic-IT80g2018/src/app/app.constants.ts
+++
@@ -1,5 +0,0 @@
-export const FAKULTET_URL = 'http://localhost:8084/fakultet';
-export const STATUS_URL = 'http://localhost:8084/status';
-export const DEPARTMAN_URL = 'http://localhost:8084/departman';
-export const STUDENT_URL = 'http://localhost:8084/student';
-export const STUDENT_DEPARTMAN_URL = 'http://localhost:8084/studentDepartman';
